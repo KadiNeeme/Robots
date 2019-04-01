4c66d4543e11f4b206bbca69cf2dc32333a4ca78
Kadi <kadi.neeme@gmail.com>
2019-04-01 22:02:29 +0300

    Updates

---
+++ precache-manifest.19b0c04fe76cd178d34b38e9fa8cf6f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffbcd59c6e3925752947",
+    "url": "/Robots/static/css/main.a76a7a89.chunk.css"
+  },
+  {
+    "revision": "ffbcd59c6e3925752947",
+    "url": "/Robots/static/js/main.9ad172c7.chunk.js"
+  },
+  {
+    "revision": "95f51dd4f824221f6d09",
+    "url": "/Robots/static/js/runtime~main.ec77e159.js"
+  },
+  {
+    "revision": "782c2756b7bca998911c",
+    "url": "/Robots/static/css/2.ac6682ef.chunk.css"
+  },
+  {
+    "revision": "782c2756b7bca998911c",
+    "url": "/Robots/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c089ba5050e52afb1ff26ab087e759e9",
+    "url": "/Robots/index.html"
+  }
+];
